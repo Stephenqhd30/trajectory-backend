09972d7d809a45aa162ac0402c40688d7a3a4fa8
Stephenqhd30 <Popcornqhd@gmail.com>
2024-12-18 22:19:40 +0800

    refactor(jobs):更改定时任务中更新数据的频率 feat(Readme.md): 更新readme.md

--- src/main/java/com/stephen/trajectory/job/cycle/IncSyncUserToEs.java
+++ src/main/java/com/stephen/trajectory/job/cycle/IncSyncUserToEs.java
@@ -33,7 +33,7 @@ public class IncSyncUserToEs {
 	/**
 	 * 每3分钟执行一次
 	 */
-	@Scheduled(fixedRate = 3 * 60 * 1000)
+	@Scheduled(fixedRate = 60 * 1000)
 	public void run() {
 		// 查询近 5 分钟内的数据
 		Date fiveMinutesAgoDate = new Date(new Date().getTime() - 5 * 60 * 1000L);
